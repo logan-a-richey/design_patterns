6b8fa877266a58d24e0920322d5cf9dcc6a35c69
logan-a-richey <loganricheygithub@gmail.com>
2025-05-25 17:01:44 -0500

    added command

--- Behavioral/command.cpp
+++ Behavioral/command.cpp
@@ -1,1 +1,78 @@
 // command.cpp
+
+#include <iostream>
+
+/*
+--- Problem statement --- 
+Design a system that demonstrates the use of the Command Pattern to decouple the
+sender and receiver of a request. The system should consist of several key 
+components: Command, Concrete Command, Receiver, and Invoker.
+*/
+
+// Receiver
+class Receiver {
+public:
+    // Receiver class defines the action to be performed.
+    void performAction() {
+        std::cout << "Receiver is performing an action" << std::endl;
+    }
+};
+
+// Command interface
+class Command {
+public:
+    // The execute method is declared in the Command interface.
+    virtual void execute() = 0;
+};
+
+// Concrete Command
+class ConcreteCommand : public Command {
+private:
+    Receiver &receiver;
+
+public:
+    // ConcreteCommand takes a reference to a Receiver
+    // object in its constructor.
+    ConcreteCommand(Receiver&rec) : receiver(rec) {}
+
+    // The execute method calls the action on the Receiver.
+    void execute() {
+        receiver.performAction(); 
+    }
+};
+
+// Invoker
+class Invoker {
+private:
+    Command* command;
+
+public:
+    // The setCommand method allows setting the command to be executed.
+    void setCommand(Command* cmd) {
+        command = cmd; 
+    }
+
+    // The executeCommand method triggers the execution of the command.
+    void executeCommand() { 
+        command->execute(); 
+    }
+};
+
+int main() {
+    // Create a Receiver instance.
+    Receiver receiver;
+
+    // Create a ConcreteCommand, passing the Receiver to it.
+    ConcreteCommand command(receiver);
+
+    // Create an Invoker.
+    Invoker invoker;
+
+    // Set the command to be executed by the invoker.
+    invoker.setCommand(&command);
+
+    // Execute the command.
+    invoker.executeCommand();
+
+    return 0;
+}
